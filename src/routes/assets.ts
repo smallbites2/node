7acd8d418cb86a390fd597903bf3dcf4b795f146
pixel <pixel@fireplaceai.com>
2025-01-18 01:42:03 +0100

    Replace gm with sharp, a faster alternative.

--- src/routes/assets.ts
+++ src/routes/assets.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express';
 import { s3 } from '../env';
 import { minioClient } from '../s3';
-import gm from 'gm';
-import fs from 'node:fs';
+import sharp from 'sharp';
 
 const router = Router();
 
@@ -16,7 +15,8 @@ router.get('/assets/food/:id.webp', async (req, res) => {
       if (!isNaN(size) && size > 1 && size < 1024) {
         res.header('Content-Type', 'image/webp');
         //res.header("Cache-Control", "public, max-age=31536000");
-        gm(stream).resize(size).setFormat('webp').quality(80).stream().pipe(res);
+        const transformer = sharp().resize(size).webp({ quality: 80 });
+        stream.pipe(transformer).pipe(res);
         return;
       }
     }
